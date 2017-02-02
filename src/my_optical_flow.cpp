9b6c6b9d179b07c650b138fa00807bc44588cba5
Lawrencio <rasamoelis@eisti.eu>
2017-02-02 13:38:23 +0100

    ca semble marcher

--- src/my_optical_flow.cpp
+++ src/my_optical_flow.cpp
@@ -10,39 +10,17 @@
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 
-#define ITER_NB 5
+#define ITER_NB 50
 
-// Access red, green, blue, and alpha component values in a 32-bit unsigned RGBA $
-
-inline unsigned int RED(unsigned int pixel)
-{
-	return pixel >> 24;
-}
-
-inline unsigned int GREEN(unsigned int pixel)
-{
-	return (pixel >> 16) & 0x000000FF;
-}
-
-inline unsigned int BLUE(unsigned int pixel)
-{
-	return (pixel >> 8) & 0x000000FF;
-}
-
-inline unsigned int ALPHA(unsigned int pixel)
-{
-	return pixel & 0x000000FF;
-}
-
+// Access red, green, blue, and alpha component values in a 32-bit unsigned RG$
+#define ALPHA(pixel)  ( (pixel)>>24)
+#define BLUE(pixel)   (((pixel)>>16) & 0xFF)
+#define GREEN(pixel)  (((pixel)>>8)  & 0xFF)
+#define RED(pixel)    ( (pixel)      & 0xFF)
 
 // Encode a 32-bit unsigned RGBA value from individual
-
 // red, green, blue, and alpha component values.
-
-inline unsigned int RGBA(unsigned int r, unsigned int g, unsigned int b, unsigned int a)
-{
- 	return ((((r) << 24)) | (((g) << 16)) | (((b) << 8)) | ((a)));
-}
+#define RGBA(r,g,b,a) ((((a) << 24)) | (((b) << 16)) | (((g) << 8)) | ((r)))
 
 
 void rgba_to_gray(unsigned int* out, unsigned int* img, size_t img_size)
@@ -55,12 +33,12 @@ void rgba_to_gray(unsigned int* out, unsigned int* img, size_t img_size)
 		p = img[i];
 		m = RED(p) + GREEN(p) + BLUE(p);
 		m /= 3;
-		out[i] = RGBA(m, m, m, ALPHA(p));
+		out[i] = RGBA(m, m, m, 255);
 	}
 	p = img[0];
 	m = RED(p) + GREEN(p) + BLUE(p);
 	m /= 3;
-	out[0] = RGBA(m, m, m, ALPHA(p));
+	out[0] = RGBA(m, m, m, 255);
 }
 
 void rgba_to_intensity(unsigned char* out, unsigned int* img, size_t img_size)
@@ -89,10 +67,10 @@ void intensity_to_rgba(unsigned int* out, unsigned char* img, size_t img_size)
 	for(i = img_size_1; i!=0; i--)
 	{
 		p = (unsigned int)(img[i]);
-		out[i] = RGBA(p, p, p, p);
+		out[i] = RGBA(p, p, p, 255);
 	}
 	p = img[0];
-	out[0] = RGBA(p, p, p, p);
+	out[0] = RGBA(p, p, p, 255);
 }
 
 
@@ -115,8 +93,8 @@ void optical_flow(unsigned int* out, unsigned char* It1, unsigned char* It2, int
 	float pt1_c, pt1_d, pt1_b, pt1_bd;
 	float v_Ix, v_Iy;
 
-	float U[wh + 2 * w + 2];
-	float V[wh + 2 * w + 2];
+	float U[wh];
+	float V[wh];
 	float Ix[wh];
 	float Iy[wh];
 	float Idt[wh];
@@ -130,6 +108,14 @@ void optical_flow(unsigned int* out, unsigned char* It1, unsigned char* It2, int
 	largeur = w;
 	hauteur = h;
 
+	unsigned int p1;
+	unsigned int plargeur;
+	unsigned int p1largeur;
+	unsigned int plargeur_1;
+	unsigned int p_largeur_1;
+	unsigned int p_largeur;
+	unsigned int p_1;
+
 
 	//printf("bordel\n");
 
@@ -139,24 +125,27 @@ void optical_flow(unsigned int* out, unsigned char* It1, unsigned char* It2, int
 	{
 //		printf("%d\n", p);
 
+		p1 = p == wh_1 ? p : p + 1;
+		plargeur = p <= wh_1 - largeur ? p + largeur : p;
+		p1largeur = p <= wh_1 - largeur - 1 ? p + largeur + 1 : p;
+
 		pt_c = (float)(It1[p]);
-		pt_d = (float)(It1[p + 1]);
-		pt_b = (float)(It1[p + largeur]);
-		pt_bd = (float)(It1[p + 1 + largeur]);
+		pt_d = (float)(It1[p1]);
+		pt_b = (float)(It1[plargeur]);
+		pt_bd = (float)(It1[p1largeur]);
 
 
 		pt1_c = (float)(It2[p]);
-		pt1_d = (float)(It2[p + 1]);
-		pt1_b = (float)(It2[p + largeur]);
-		pt1_bd = (float)(It2[p + 1 + largeur]);
+		pt1_d = (float)(It2[p1]);
+		pt1_b = (float)(It2[plargeur]);
+		pt1_bd = (float)(It2[p1largeur]);
 
 
 		Iy[p] = ((pt_bd + pt_b - pt_c - pt_d) + (pt1_bd + pt1_b - pt1_c - pt1_d)) * div_coeff;
 
-		Ix[p] = ((pt_d + pt_bd - pt_c - pt_b) + (pt1_d + pt1_bd - pt1_c - pt1_b)) * div_coeff;
+		Ix[p] = ((pt_d + pt_bd - pt_c - pt_b) + (pt1_d + pt1_bd - pt1_c - pt1_b))  * div_coeff;
 
 		Idt[p] = ((pt1_c + pt1_b + pt1_d + pt1_bd) - (pt_c + pt_b + pt_d + pt_bd)) * div_coeff;
-
 
 	}
 
@@ -164,34 +153,38 @@ void optical_flow(unsigned int* out, unsigned char* It1, unsigned char* It2, int
 	div_coeff2 = 1. / 6.;
 	for (iter = nb_iterations; iter--;)
 	{
-		for (i = wh_1 ; i >= 0 ; i--)
+		for (p = wh_1 ; p >= 0 ; p--)
 		{
-/*			p = i + largeur + 1;
+			p1 = p == wh_1 ? p : p + 1;
+			plargeur = p <= wh_1 - largeur ? p + largeur : p;
+			p1largeur = p <= wh_1 - largeur - 1 ? p + largeur + 1 : p;
+			plargeur_1 = p <= wh_1 - largeur + 1 ? p -1 + largeur : p;
+			p_largeur_1 = p >= largeur + 1 ? p - largeur -1 : p;
+			p_largeur = p >= largeur ? p - largeur : p;
+			p_1 = p >= 1 ? p - 1 : p;
 //			printf("boucle 2 %d\n",p);
 //			printf("%d %d\n", U[p], V[p]);
 
-			M_u = (U[p - largeur - 1] + U[p - largeur + 1] + U[p + largeur - 1] + U[p + largeur + 1]) * div_coeff + (U[p - largeur] + U[p - 1] + U[p + 1] + U[p + largeur]) * div_coeff2;
-			M_v = (V[p - largeur - 1] + V[p - largeur + 1] + V[p + largeur - 1] + V[p + largeur + 1]) * div_coeff + (V[p - largeur] + V[p - 1] + V[p + 1] + V[p + largeur]) * div_coeff2;
+			M_u = (U[p_largeur_1] + U[p_largeur + 1] + U[plargeur_1] + U[p1largeur]) * div_coeff + (U[p_largeur] + U[p_1] + U[p1] + U[p_largeur])  * div_coeff2;
+			M_v = (V[p_largeur_1] + V[p_largeur + 1] + V[plargeur_1] + V[p1largeur]) * div_coeff  + (V[p_largeur] + V[p_1] + V[p_1] + V[p_largeur])  * div_coeff2;
 
-			v_Ix = Ix[i];
-			v_Iy = Iy[i];
-			U[p] = M_u - ((v_Ix * (v_Ix * M_u + v_Iy * M_v + Idt[i])) / (alpha + v_Ix * v_Ix + v_Iy * v_Iy));
-			V[p] = M_v - ((v_Iy * (v_Ix * M_u + v_Iy * M_v + Idt[i])) / (alpha + v_Ix * v_Ix + v_Iy * v_Iy));
+			v_Ix = Ix[p];
+			v_Iy = Iy[p];
+			U[p] = M_u - ((v_Ix * (v_Ix * M_u + v_Iy * M_v + Idt[p])) / (alpha + v_Ix * v_Ix + v_Iy * v_Iy));
+			V[p] = M_v - ((v_Iy * (v_Ix * M_u + v_Iy * M_v + Idt[p])) / (alpha + v_Ix * v_Ix + v_Iy * v_Iy));
 
-*/
 
 		}
 	}
 
-/*	int wh_largeur_1 = wh + largeur + 1;
-	for (p = largeur + 1; p < wh_largeur_1; p++)
+	for (p = wh_1; p >=0 ; p--)
 	{
-		unsigned char red = (unsigned char) ((U[p] + 0.5f) * 255.f);
-		unsigned char blue = (unsigned char)((V[p] + 0.5f) * 255.f);
-		out[p] = RGBA(RED(c), GREEN(0), BLUE(blue), ALPHA(255));
-		printf("%f, %f\n", U[p], V[p]);
-		}
-	}*/
+		unsigned char red = (unsigned char)((U[p] * 3.5f) * + 127.f);
+		unsigned char blue = (unsigned char)((V[p] * 3.5f) * + 127.f);
+		out[p] = RGBA(red, 0, blue, 255);
+//		printf("p: %d, %f, %f\n", p, U[p], V[p]);
+//		printf("%d %d\n", red, blue);
+	}
 
 
 	//memcpy(It1, It, largeur * hauteur * sizeof(unsigned char));
@@ -219,9 +212,11 @@ int main(int argc, char **argv)
 
 	unsigned int img1_data_t[wh];
 	unsigned int img2_data_t[wh];
-	unsigned char img1_data[wh + 8];
-	unsigned char img2_data[wh + 8];
-	unsigned int out_data[wh];
+	unsigned char* img1_data = (unsigned char*)malloc(wh * sizeof(unsigned char));
+	unsigned char* img2_data = (unsigned char*)malloc(wh * sizeof(unsigned char));
+	unsigned int* out_data = (unsigned int*)malloc(img1_size);
+	//Allocating dynamically to avoid stackoverflow on very large images...
+	//and avoid moving megabits of stack context
 
 	memcpy(img1_data_t, img1.getPixelsPtr(), img1_size);
 	memcpy(img2_data_t, img2.getPixelsPtr(), img1_size);
@@ -237,6 +232,9 @@ int main(int argc, char **argv)
 	sf::Image out;
 	out.create(w, h, (const unsigned char *)out_data);
 	out.saveToFile(outfile);
+	free(img1_data);
+	free(img2_data);
+	free(out_data);
 
 	return 0;
 }
