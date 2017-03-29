2618f30ca3e57a1dae79810729aa7b8eba0a72c6
Benjamin Demailly <benjamin.demailly@eisti.eu>
2017-03-29 23:24:05 +0200

    sauvegarde avant un pull

--- src/moc_mainwindow.cpp
+++ src/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,34 +12,35 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.3.2. It"
+#error "This file was generated using the moc from 5.2.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[7];
-    char stringdata[90];
+    QByteArrayData data[8];
+    char stringdata[91];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData)) \
+    offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
     )
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10),
 QT_MOC_LITERAL(1, 11, 15),
 QT_MOC_LITERAL(2, 27, 0),
-QT_MOC_LITERAL(3, 28, 15),
-QT_MOC_LITERAL(4, 44, 19),
-QT_MOC_LITERAL(5, 64, 10),
-QT_MOC_LITERAL(6, 75, 14)
+QT_MOC_LITERAL(3, 28, 19),
+QT_MOC_LITERAL(4, 48, 10),
+QT_MOC_LITERAL(5, 59, 9),
+QT_MOC_LITERAL(6, 69, 9),
+QT_MOC_LITERAL(7, 79, 10)
     },
-    "MainWindow\0movieManagement\0\0closeExperience\0"
-    "configNaoConnection\0connectNao\0"
-    "dataManagement"
+    "MainWindow\0closeExperience\0\0"
+    "configNaoConnection\0connectNao\0dataClick\0"
+    "bothClick\0movieClick\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +50,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,13 +58,15 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    0,   40,    2, 0x08 /* Private */,
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
+       1,    0,   44,    2, 0x08,
+       3,    0,   45,    2, 0x08,
+       4,    0,   46,    2, 0x08,
+       5,    0,   47,    2, 0x08,
+       6,    0,   48,    2, 0x08,
+       7,    0,   49,    2, 0x08,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -78,11 +81,12 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
     if (_c == QMetaObject::InvokeMetaMethod) {
         MainWindow *_t = static_cast<MainWindow *>(_o);
         switch (_id) {
-        case 0: _t->movieManagement(); break;
-        case 1: _t->closeExperience(); break;
-        case 2: _t->configNaoConnection(); break;
-        case 3: _t->connectNao(); break;
-        case 4: _t->dataManagement(); break;
+        case 0: _t->closeExperience(); break;
+        case 1: _t->configNaoConnection(); break;
+        case 2: _t->connectNao(); break;
+        case 3: _t->dataClick(); break;
+        case 4: _t->bothClick(); break;
+        case 5: _t->movieClick(); break;
         default: ;
         }
     }
@@ -114,13 +118,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 6;
     }
     return _id;
 }
